4562f878dcff63d74a5e0f6c757815865e703463
George Bradley <77509864+Enantiodromis@users.noreply.github.com>
2024-08-19 12:59:19 +0100

    simplify count

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
-var run = countUpFromTime('1998-07-31 06:00:00', 'countup1');
+var run = countUpFromTime('1998-07-31', 'countup1');
 window.onload = run
 
 function countUpFromTime(countFrom, id) {
@@ -13,28 +13,16 @@ function countUpFromTime(countFrom, id) {
     
     var timeDifference = now - countFromDate;
 
-    var secondsInAMinute = 60;
-    var secondsInAnHour = 60 * 60;
-    var secondsInADay = 24 * 60 * 60;
-    var secondsInAYear = 365.25 * 24 * 60 * 60;
+    var secondsInADay = 24 * 60 * 60 * 1000; // milliseconds in a day
+    var secondsInAYear = 365.25 * secondsInADay; // average milliseconds in a year
 
-    var years = Math.floor(timeDifference / (secondsInAYear * 1000));
-    var remainingAfterYears = timeDifference % (secondsInAYear * 1000);
-    var days = Math.floor(remainingAfterYears / (secondsInADay * 1000));
-    var remainingAfterDays = remainingAfterYears % (secondsInADay * 1000);
-    var hours = Math.floor(remainingAfterDays / (secondsInAnHour * 1000));
-    var remainingAfterHours = remainingAfterDays % (secondsInAnHour * 1000);
-    var minutes = Math.floor(remainingAfterHours / (secondsInAMinute * 1000));
-    var seconds = Math.floor((remainingAfterHours % (secondsInAMinute * 1000)) / 1000);
+    var years = Math.floor(timeDifference / secondsInAYear);
+    var days = Math.floor((timeDifference % secondsInAYear) / secondsInADay);
 
     var idEl = document.getElementById(id);
     idEl.getElementsByClassName('years')[0].innerHTML = years;
     idEl.getElementsByClassName('days')[0].innerHTML = days;
-    idEl.getElementsByClassName('hours')[0].innerHTML = hours;
-    idEl.getElementsByClassName('minutes')[0].innerHTML = minutes;
-    idEl.getElementsByClassName('seconds')[0].innerHTML = seconds;
 
     clearTimeout(countUpFromTime.interval);
     countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
 }
-
