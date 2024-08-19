7106d19285d10fa9792b8d9415b79d9d65f7bc72
George Bradley <77509864+Enantiodromis@users.noreply.github.com>
2024-08-19 12:50:09 +0100

    updating age calculator

--- js/script.js
+++ js/script.js
@@ -8,20 +8,21 @@ var run = countUpFromTime('1998-07-31 06:00:00', 'countup1');
 window.onload = run
 
 function countUpFromTime(countFrom, id) {
-    countFrom = new Date(countFrom).getTime();
+    var countFromDate = new Date(countFrom).getTime();
     var now = new Date().getTime();
     
-    var timeDifference = now - countFrom;
-    
-    var secondsInADay = 60 * 60 * 24,
-        secondsInAnHour = 60 * 60,
-        secondsInAMinute = 60;
+    var timeDifference = now - countFromDate;
 
-    var years = Math.floor(timeDifference / (secondsInADay * 365.25 * 1000)),
-        days = Math.floor((timeDifference % (secondsInADay * 365.25 * 1000)) / (secondsInADay * 1000)),
-        hours = Math.floor((timeDifference % (secondsInADay * 1000)) / (secondsInAnHour * 1000)),
-        minutes = Math.floor((timeDifference % (secondsInAnHour * 1000)) / (secondsInAMinute * 1000)),
-        seconds = Math.floor((timeDifference % (secondsInAMinute * 1000)) / 1000);
+    var secondsInAMinute = 60;
+    var secondsInAnHour = 60 * 60;
+    var secondsInADay = 24 * 60 * 60;
+    var secondsInAYear = 365.25 * 24 * 60 * 60;
+
+    var years = Math.floor(timeDifference / (secondsInAYear * 1000));
+    var days = Math.floor((timeDifference % (secondsInAYear * 1000)) / (secondsInADay * 1000));
+    var hours = Math.floor((timeDifference % (secondsInADay * 1000)) / (secondsInAnHour * 1000));
+    var minutes = Math.floor((timeDifference % (secondsInAnHour * 1000)) / (secondsInAMinute * 1000));
+    var seconds = Math.floor((timeDifference % (secondsInAMinute * 1000)) / 1000);
 
     var idEl = document.getElementById(id);
     idEl.getElementsByClassName('years')[0].innerHTML = years;
