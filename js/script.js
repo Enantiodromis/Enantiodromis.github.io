96947ba88a683cee99c54271b3a8167b8d8164c3
George Bradley <77509864+Enantiodromis@users.noreply.github.com>
2024-08-19 12:30:53 +0100

    Update count

--- js/script.js
+++ js/script.js
@@ -8,28 +8,28 @@ var run = countUpFromTime("Jul 31, 1998 06:00:00", 'countup1');
 window.onload = run
 
 function countUpFromTime(countFrom, id) {
-  countFrom = new Date(countFrom).getTime();
-  var now = new Date(),
-      countFrom = new Date(countFrom),
-      timeDifference = (now - countFrom);
+    countFrom = new Date(countFrom).getTime();
+    var now = new Date().getTime();
     
-  var secondsInADay = 60 * 60 * 1000 * 24,
-      secondsInAHour = 60 * 60 * 1000;
+    var timeDifference = now - countFrom;
     
-  days = Math.floor(timeDifference / (secondsInADay) * 1);
-  years = Math.floor(days / 365);
-  if (years > 1){ days = days - (years * 365) }
-  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+    var secondsInADay = 60 * 60 * 24,
+        secondsInAnHour = 60 * 60,
+        secondsInAMinute = 60;
 
-  var idEl = document.getElementById(id);
-  idEl.getElementsByClassName('years')[0].innerHTML = years;
-  idEl.getElementsByClassName('days')[0].innerHTML = days;
-  idEl.getElementsByClassName('hours')[0].innerHTML = hours;
-  idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
-  idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+    var years = Math.floor(timeDifference / (secondsInADay * 365.25 * 1000)),
+        days = Math.floor((timeDifference % (secondsInADay * 365.25 * 1000)) / (secondsInADay * 1000)),
+        hours = Math.floor((timeDifference % (secondsInADay * 1000)) / (secondsInAnHour * 1000)),
+        minutes = Math.floor((timeDifference % (secondsInAnHour * 1000)) / (secondsInAMinute * 1000)),
+        seconds = Math.floor((timeDifference % (secondsInAMinute * 1000)) / 1000);
 
-  clearTimeout(countUpFromTime.interval);
-  countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+    var idEl = document.getElementById(id);
+    idEl.getElementsByClassName('years')[0].innerHTML = years;
+    idEl.getElementsByClassName('days')[0].innerHTML = days;
+    idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+    idEl.getElementsByClassName('minutes')[0].innerHTML = minutes;
+    idEl.getElementsByClassName('seconds')[0].innerHTML = seconds;
+
+    clearTimeout(countUpFromTime.interval);
+    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
 }
