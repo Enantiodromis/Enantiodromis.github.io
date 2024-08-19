818e1b8a3dad74fb5fd09fe91cec69b9c6382086
George Bradley <77509864+Enantiodromis@users.noreply.github.com>
2024-08-19 13:02:43 +0100

    revert to seconds

--- js/script.js
+++ js/script.js
@@ -4,25 +4,32 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
-var run = countUpFromTime('1998-07-31', 'countup1');
+var run = countUpFromTime('1998-07-31 06:00:00', 'countup1');
 window.onload = run
 
 function countUpFromTime(countFrom, id) {
-    var countFromDate = new Date(countFrom).getTime();
-    var now = new Date().getTime();
+  countFrom = new Date(countFrom).getTime();
+  var now = new Date(),
+      countFrom = new Date(countFrom),
+      timeDifference = (now - countFrom);
     
-    var timeDifference = now - countFromDate;
+  var secondsInADay = 60 * 60 * 1000 * 24,
+      secondsInAHour = 60 * 60 * 1000;
+    
+  days = Math.floor(timeDifference / (secondsInADay) * 1);
+  years = Math.floor(days / 365);
+  if (years > 1){ days = days - (years * 365) }
+  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
 
-    var secondsInADay = 24 * 60 * 60 * 1000; // milliseconds in a day
-    var secondsInAYear = 365.25 * secondsInADay; // average milliseconds in a year
+  var idEl = document.getElementById(id);
+  idEl.getElementsByClassName('years')[0].innerHTML = years;
+  idEl.getElementsByClassName('days')[0].innerHTML = days;
+  idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+  idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+  idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
 
-    var years = Math.floor(timeDifference / secondsInAYear);
-    var days = Math.floor((timeDifference % secondsInAYear) / secondsInADay);
-
-    var idEl = document.getElementById(id);
-    idEl.getElementsByClassName('years')[0].innerHTML = years;
-    idEl.getElementsByClassName('days')[0].innerHTML = days;
-
-    clearTimeout(countUpFromTime.interval);
-    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+  clearTimeout(countUpFromTime.interval);
+  countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
 }
