828237a5a3d9862b06208990fb39eca3696d5eeb
Enantiodromis <georgerebradley@gmail.com>
2024-08-19 13:55:53 +0100

    Corrected age calculation

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ window.onload = run
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
   var now = new Date(),
-      countFrom = new Date(countFrom),
-      timeDifference = (now - countFrom);
+      timeDifference = now - countFrom;
     
   var secondsInADay = 60 * 60 * 1000 * 24,
       secondsInAHour = 60 * 60 * 1000;
     
-  days = Math.floor(timeDifference / (secondsInADay) * 1);
-  years = Math.floor(days / 365);
-  if (years > 1){ days = days - (years * 365) }
-  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+  days = Math.floor(timeDifference / secondsInADay);
+  years = Math.floor(days / 365.25);  // 365.25 to account for leap years
+  days = Math.floor(days % 365.25);   // Days remaining after full years
+  
+  hours = Math.floor((timeDifference % secondsInADay) / secondsInAHour);
+  mins = Math.floor(((timeDifference % secondsInADay) % secondsInAHour) / (60 * 1000));
+  secs = Math.floor((((timeDifference % secondsInADay) % secondsInAHour) % (60 * 1000)) / 1000);
 
   var idEl = document.getElementById(id);
   idEl.getElementsByClassName('years')[0].innerHTML = years;
